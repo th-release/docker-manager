a19bd12464ccfea5d4dc8944bfb2fdcd57a81500
th-release <cth.release@gmail.com>
2024-12-17 08:23:09 +0900

    feat: Added Set Port

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"cth-core.xyz/docker-manager/utils"
 	"cth-core.xyz/docker-manager/web"
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,16 +14,16 @@ func main() {
 
 	web.SetupRoute(app)
 
-	// port, err := utils.GetEnv("PORT")
+	port, err := utils.GetEnv("PORT")
 
-	// if err != nil {
-	// 	log.Print("ENV: PORT를 불러올 수 없습니다.")
-	// 	os.Exit(1)
-	// }
+	if err != nil {
+		log.Print("ENV: PORT를 불러올 수 없습니다.")
+		os.Exit(1)
+	}
 
-	// if len(port) <= 0 {
-	// 	log.Print("웹 서버 실행 오류: PORT 변수가 잘못 입력되었습니다.")
-	// 	os.Exit(1)
-	// }
-	app.Listen(":" + "8081")
+	if len(port) <= 0 {
+		log.Print("웹 서버 실행 오류: PORT 변수가 잘못 입력되었습니다.")
+		os.Exit(1)
+	}
+	app.Listen(":" + port)
 }
